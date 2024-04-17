4bd4480d6f8339e25108145e47d5008329da8571
dexie812 <mypc76239@gmail.com>
2024-04-17 11:49:48 -0400

    UseState Hook in practice.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Practice from './Practice.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Practice></Practice>
+    </div>
+  );
+}
+
+export default App;
